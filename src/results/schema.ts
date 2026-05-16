7567d23dd2d941cc781a530129d182645b0e89ef
David <Davidlomath@hotmail.co.uk>
2026-05-17 00:36:14 +0100

    add missing types

---
+++ src/results/schema.ts
@@ -0,0 +1,13 @@
+export type {
+  AttemptResult,
+  BenchmarkResult,
+  CommandResult,
+  EvaluationResult,
+  EvaluationStatus,
+  HumanReviewResult,
+  HumanReviewStatus,
+  ModelResult,
+  ShiptestRunnerResult,
+  ShiptestRunResult,
+  SnapshotResult,
+} from "./types.js";
